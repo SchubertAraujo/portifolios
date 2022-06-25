67763a05661826003b01bf19a5d9f9f04b7dbec8
Schubert Araujo <xubsoldier@gmail.com>
2022-06-25 17:39:49 -0300

    funcionalidades da calculadora adicionadas

--- Calculadora/assets/js/script.js
+++ Calculadora/assets/js/script.js
@@ -2,14 +2,52 @@ function getNumber(value){
     document.getElementById('display').value += value   
 }
 
-function getOperation(operator){
+function getOperation(action){
+    clearAction = action == 'clear'
+    equalAction = action == '='
+    zeroValue = document.getElementById('display').value == '0'
+    emptyValue = document.getElementById('display').value == ''
+    endsinOperator = endsWithOperator(document.getElementById('display').value)
 
-    if (operator == 'clear') 
-        document.getElementById('display').value = ''
-    else if(operator == '=') 
-        eval(document.getElementById('display').value)
-    else 
-        document.getElementById('display').value += operator   
+    let result = document.getElementById('display').value;
+  
+    if (clearAction) 
+        result = ''
+    else if(equalAction) 
+        result = handlingEval(document.getElementById('display').value)
+    else if (!zeroValue && !emptyValue && !endsOperator) 
+        result = document.getElementById('display').value + action   
+
+    document.getElementById('display').value = result
+}
 
 
+function handlingEval(operation) {
+    try {
+        
+       if (eval(operation) == Infinity) {
+            alert('Não é possível fazer divisão por zero')
+            return '0'
+       }
+       return eval(operation)
+
+    } catch(e) {        
+        let clearNumber = operation
+        if (endsWithOperator(clearNumber)){
+            clearNumber = operation.slice(0 , -1)
+        }
+
+        return eval(clearNumber)
+    }
+}
+
+function endsWithOperator(expression){
+    let minus = expression.endsWith('-')
+    let plus = expression.endsWith('+')
+    let time = expression.endsWith('*')
+    let dividedBy = expression.endsWith('/')
+    if (minus || plus || time || dividedBy){
+        return true
+    }
+    return false
 }
